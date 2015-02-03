c33528f4562747c4ff29951e4b57ac84cfdb7372
vaso voo koh teach <sova.kuliana@gmail.com>
2015-02-03 13:36:48 -0500

    blurb cloning, now with skittles.

--- src/pachax/views/global.clj
+++ src/pachax/views/global.clj
@@ -22,7 +22,7 @@
 (def blurb-sample-content '({:tag :div, :content ["the art of paper folding is an ancient one..."]}))
 
 (def blurb-content-transform
-  (eh/transform global-page [:#blurb001] (eh/clone-for [i (range 4)] (eh/content blurb-sample-content (str i)))))
+  (eh/transform global-page [:#blurb001] (eh/clone-for [i (range 15)] (eh/content blurb-sample-content (str i)))))
 
 ;eh/content blurb-sample-content)))
 
