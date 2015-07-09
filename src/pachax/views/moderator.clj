f16be1f42328247647c11a76c567cb101d6fd2b4
vaso voo koh teach <sova.kuliana@gmail.com>
2015-07-09 11:32:56 -0400

    enlive did not like raw FID as a Long.  well whatever, it works now.  time to tidy up the feedback page.

--- src/pachax/views/moderator.clj
+++ src/pachax/views/moderator.clj
@@ -6,47 +6,38 @@
 ;;replace blurb contents 
 (def moderator-page (eh/html-resource "moderator.html"))
 
-(def number-of-feedbacks-to-show 100)
-
 ;;feedback area populating
-(defn feedback-sample-content [feedbackID feedback-map]
-  (let [fid (:fid feedback-map)
-        sender (:email feedback-map)
-        content (:content feedback-map)]
-    (list
-     {:tag :div, 
-      :attrs {:id (str "feedback" feedbackID)
-              :class "feedbackin"}
-      :content (list 
-                {:tag :div,
-                 :attrs {:id (str "feedback-sender" feedbackID),
-                         :class (str "inner-feedback-sender")}
-                 :content sender},
-                {:tag :div,
-                 :attrs {:id (str "feedback-content" feedbackID),
-                         :class (str "inner-feedback-content")},
-                 :content content},
-                {:tag :div,
-                 :attrs {:id (str "feedback-fid" feedbackID),
-                         :class (str "inner-feedback-fid")}
-                 :content fid})})))
+(defn feedback-entry [feedback-map]
+ (let [fid (:fid feedback-map)
+      sender (:email feedback-map)
+      feedback-content (:content feedback-map)]
+   (list 
+    {:tag :div
+     :attrs {:class "feedbacks"}
+     :content (list ;;(str fid sender feedback-content))})))
+               {:tag :div
+                :attrs {:class "inner-feedback-sender"}
+                :content sender},
+               {:tag :div
+                :attrs {:class "inner-feedback-content"}
+                :content feedback-content}
+               {:tag :div
+                :attrs {:class "inner-feedback-fid"}
+                :content (str fid)})}))) ;;enlive does not like raw Longs. and therefore neds a str wrapper on this fid
 
 (defn feedback-content-transform []
   (let [feedback-area (eh/select moderator-page [:.feedback])
-        get-unread-feedback (dbm/get-unread-feedback)
-        num-feedback (count get-unread-feedback)]
+        unread-feedbacks (dbm/get-unread-feedback)
+        number-of-feedbacks (count unread-feedbacks)]
     ;;takes the first [only] element named .feedback, clones it, fills it with goodness
     (eh/transform feedback-area [:.feedback]
-       (eh/clone-for [i num-feedback]
-         (eh/content 
-           (feedback-sample-content i ;;loop over every feedback
-                                    (nth get-unread-feedback i)))))))
+       (eh/clone-for [i (range number-of-feedbacks)]
+          (let [fid (:fid (nth unread-feedbacks i))]
+            (eh/content (feedback-entry (nth unread-feedbacks i))))))))
+             ;; works : (str (nth unread-feedbacks i))))))))
 
 (defn moderator-page-draw [ email ]
   (apply str (eh/emit* 
               (eh/at moderator-page 
-                     [:.feedback]    (eh/substitute (feedback-content-transform))
-                     ;[:.brief]    (eh/substitute (brief-content-transform))
-                     [:.usercard] (eh/substitute (pvu/usercard-transform moderator-page email))
-                      ;vine transforms
-                     ))))
+                     [:.feedback] (eh/substitute (feedback-content-transform))
+                     [:.usercard] (eh/substitute (pvu/usercard-transform moderator-page email))))))
